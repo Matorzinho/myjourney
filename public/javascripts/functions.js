d35150b0bf4e4b07b6b121d55430a3b72ee62f83
Doug <dougbacelar@gmail.com>
2016-02-18 21:27:52 +0000

    menu transition and some style

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-  $('.slider').slider({interval: 900000});
+  $('.slider').slider({interval: 9000});
+  $(".button-collapse").sideNav();
+});
+jQuery(window).scroll(function(){
+    if(jQuery(window).scrollTop() > 50){
+        jQuery('nav').addClass('scrolled');
+    }else{
+        jQuery('nav').removeClass('scrolled');
+    }
 });
