769cefac153e648ffeac4faa3f3c4807444ad531
Doug <dougbacelar@gmail.com>
2016-02-20 11:08:08 +0000

    things

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
   $('.slider').slider({interval: 9000});
   $(".button-collapse").sideNav();
+  $('.parallax').parallax();
+  $('#edu-links a').click(function(){
+  	if(!$(this).hasClass('active')){
+		$('#edu-links div').slideUp();
+		$('#edu-links a').removeClass('active');
+  		$(this).addClass('active');
+  		$($(this)[0].nextElementSibling).slideDown();
+  	}
+  });
+  	
 });
 jQuery(window).scroll(function(){
     if(jQuery(window).scrollTop() > 50){
